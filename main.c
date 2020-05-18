5c3ff55148e32a862430e38fb30f26a4e743691a
bill ryan <bill.ryan@oc.edu>
2020-05-17 23:13:00 -0500

    version 10

--- main.c
+++ main.c
@@ -1,76 +1,121 @@
-#include <wiringPiI2C.h>
-#include <wiringPi.h>
-#include <stdio.h>
-#include <math.h>
-#include "cls.h"
- 
-int fd;
-int acclX, acclY, acclZ;
-int gyroX, gyroY, gyroZ;
-double acclX_scaled, acclY_scaled, acclZ_scaled;
-double gyroX_scaled, gyroY_scaled, gyroZ_scaled;
- 
- 
-int read_word_2c(int addr)
-{
-int val;
-val = wiringPiI2CReadReg8(fd, addr);
-val = val << 8;
-val += wiringPiI2CReadReg8(fd, addr+1);
-if (val >= 0x8000)
-val = -(65536 - val);
- 
-return val;
-}
- 
-double dist(double a, double b)
-{
-return sqrt((a*a) + (b*b));
-}
- 
-double get_y_rotation(double x, double y, double z)
-{
-double radians;
-radians = atan2(x, dist(y, z));
-return -(radians * (180.0 / M_PI));
-}
- 
-double get_x_rotation(double x, double y, double z)
-{
-double radians;
-// radians = atan2(y, dist(x, z));
-radians = atan2( y, x);
-return ((radians * (180.0 / M_PI) )  + 0.);
-}
- 
-int main()
-{
-fd = wiringPiI2CSetup (0x68);
-wiringPiI2CWriteReg8 (fd,0x6B,0x00);//disable sleep mode 6B
-printf("set 0x6B=%X\n",wiringPiI2CReadReg8 (fd,0x6B)); // 6B
- 
-while(1) 
-{
- 
-cls();
+/*
+	MPU6050 Interfacing with Raspberry Pi
+	http://www.electronicwings.com
+*/
 
-acclX = read_word_2c(0x3B);
-acclY = read_word_2c(0x3D);
-acclZ = read_word_2c(0x3F);
+#include <wiringPiI2C.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <wiringPi.h>
+#include <math.h>
+
+#define Device_Address 0x68	/*Device Address/Identifier for MPU6050*/
+
+#define PWR_MGMT_1   0x6B
+#define SMPLRT_DIV   0x19
+#define CONFIG       0x1A
+#define GYRO_CONFIG  0x1B
+#define INT_ENABLE   0x38
+#define ACCEL_XOUT_H 0x3B
+#define ACCEL_YOUT_H 0x3D
+#define ACCEL_ZOUT_H 0x3F
+#define GYRO_XOUT_H  0x43
+#define GYRO_YOUT_H  0x45
+#define GYRO_ZOUT_H  0x47
+
+int fd;
+
  
-acclX_scaled = acclX / 16384.0;
-acclY_scaled = acclY / 16384.0;
-acclZ_scaled = acclZ / 16384.0;
- 
-printf("My acclX_scaled: %f\n", acclX_scaled);
-printf("My acclY_scaled: %f\n", acclY_scaled);
-printf("My acclZ_scaled: %f\n", acclZ_scaled);
- 
-printf("My X rotation: %f\n", get_x_rotation(acclX_scaled, acclY_scaled, acclZ_scaled));
-printf("My Y rotation: %f\n", get_y_rotation(acclX_scaled, acclY_scaled, acclZ_scaled));
- 
- 
-delay(500);
+//
+//	clear screen -- must be run in PUTTY
+//
+void cls()
+{
+
+// #include <stdio.h>
+
+char FF[5]={27,'[','2','J'}; 		// the character to clear the terminal screen
+char FG[6]={27,'[',1,1,'f'};    // move the curser to the upper left of the screen
+
+
+printf("%s", FF);
+printf("%s", FG);
+
+
 }
-return 0;
+
+
+void MPU6050_Init(){
+	
+	wiringPiI2CWriteReg8 (fd, SMPLRT_DIV, 0x07);	/* Write to sample rate register */
+	wiringPiI2CWriteReg8 (fd, PWR_MGMT_1, 0x01);	/* Write to power management register */
+	wiringPiI2CWriteReg8 (fd, CONFIG, 0);		/* Write to Configuration register */
+	wiringPiI2CWriteReg8 (fd, GYRO_CONFIG, 24);	/* Write to Gyro Configuration register */
+	wiringPiI2CWriteReg8 (fd, INT_ENABLE, 0x01);	/*Write to interrupt enable register */
+
+	} 
+short read_raw_data(int addr){
+	short high_byte,low_byte,value;
+	high_byte = wiringPiI2CReadReg8(fd, addr);
+	low_byte = wiringPiI2CReadReg8(fd, addr+1);
+	value = (high_byte << 8) | low_byte;
+	return value;
 }
+
+void ms_delay(int val){
+	int i,j;
+	for(i=0;i<=val;i++)
+		for(j=0;j<1200;j++);
+}
+
+int main(){
+	
+	float Acc_x,Acc_y,Acc_z;
+	float Gyro_x,Gyro_y,Gyro_z;
+	float Ax=0, Ay=0, Az=0;
+	float Gx=0, Gy=0, Gz=0;
+	double anglex=0;
+	float dummyx=0;
+	double angley=0;
+	float dummyy=0;
+	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
+	MPU6050_Init();		                 /* Initializes MPU6050 */
+	
+	while(1)
+	{
+		cls();
+		
+		/*Read raw value of Accelerometer and gyroscope from MPU6050*/
+		Acc_x = read_raw_data(ACCEL_XOUT_H);
+		Acc_y = read_raw_data(ACCEL_YOUT_H);
+		Acc_z = read_raw_data(ACCEL_ZOUT_H);
+		
+		Gyro_x = read_raw_data(GYRO_XOUT_H);
+		Gyro_y = read_raw_data(GYRO_YOUT_H);
+		Gyro_z = read_raw_data(GYRO_ZOUT_H);
+		
+		/* Divide raw value by sensitivity scale factor */
+		Ax = Acc_x/16384.0;
+		Ay = Acc_y/16384.0;
+		Az = Acc_z/16384.0;
+		
+		Gx = Gyro_x/131;
+		Gy = Gyro_y/131;
+		Gz = Gyro_z/131;
+		
+		printf("\n Gx=%.3f °/s\tGy=%.3f °/s\tGz=%.3f °/s\tAx=%.3f g\tAy=%.3f g\tAz=%.3f g\n\n",Gx,Gy,Gz,Ax,Ay,Az);
+		
+		dummyx = Gz / Gx;
+		anglex = atan(dummyx) * 180. / M_PI;
+		dummyy = Gz / Gy;
+		angley = atan(dummyy) * 180. / M_PI;
+		printf("dummyx = %f \tanglex = %+lf\tdummyx = %f \tanglex = %+lf\n", dummyx, anglex, dummyy, angley);
+		printf("\nactual angle = %+f\n", anglex - 60.6);
+		delay(200);
+	}
+	return 0;
+}/*
+	MPU6050 Interfacing with Raspberry Pi
+	http://www.electronicwings.com
+*/
+
